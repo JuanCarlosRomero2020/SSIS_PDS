7d91414bab2f308e72dc09cfcd75eb3d1c21c67c
Juan Carlos Romero <jcrr2@ensing.edu>
2021-03-27 19:02:44 -0600

    IT 243 - Added SQL Server Agent Jobs

--- ssa_jobs/LoadDFNB3_JR.dtsx Package.sql
+++ ssa_jobs/LoadDFNB3_JR.dtsx Package.sql
@@ -1,15 +1,15 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [LoadEXM_xx.dtsx Package]    Script Date: 11/21/2019 8:31:16 PM ******/
---EXEC msdb.dbo.sp_delete_job @job_id=N'a1ace135-f5c0-4e7c-a5b9-09f69fc4bfde', @delete_unused_schedule=1
---GO
+/****** Object:  Job [LoadDFNB3_JR.dtsx Package]    Script Date: 3/27/2021 3:17:42 PM ******/
+EXEC msdb.dbo.sp_delete_job @job_id=N'a0420e16-725b-485b-88c4-1595aacade45', @delete_unused_schedule=1
+GO
 
-/****** Object:  Job [LoadEXM_xx.dtsx Package]    Script Date: 11/21/2019 8:31:16 PM ******/
+/****** Object:  Job [LoadDFNB3_JR.dtsx Package]    Script Date: 3/27/2021 3:17:42 PM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 11/21/2019 8:31:16 PM ******/
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 3/27/2021 3:17:42 PM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
@@ -18,19 +18,21 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 END
 
 DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'LoadEXM_xx.dtsx Package', 
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'LoadDFNB3_JR.dtsx Package', 
 		@enabled=1, 
 		@notify_level_eventlog=2, 
-		@notify_level_email=0, 
+		@notify_level_email=2, 
 		@notify_level_netsend=0, 
-		@notify_level_page=0, 
+		@notify_level_page=2, 
 		@delete_level=0, 
 		@description=N'This job built in support of LDS BC IT243', 
 		@category_name=N'[Uncategorized (Local)]', 
-		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+		@owner_login_name=N'sa', 
+		@notify_email_operator_name=N'Juan Carlos Romero', 
+		@notify_page_operator_name=N'Juan Carlos Romero', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Run LoadEXM_xx.dtsx from Package]    Script Date: 11/21/2019 8:31:16 PM ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Run LoadEXM_xx.dtsx from Package', 
+/****** Object:  Step [Run LoadDFNB3_xx.dtsx from Package]    Script Date: 3/27/2021 3:17:42 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Run LoadDFNB3_xx.dtsx from Package', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
@@ -40,7 +42,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Run Load
 		@retry_attempts=0, 
 		@retry_interval=0, 
 		@os_run_priority=0, @subsystem=N'SSIS', 
-		@command=N'/FILE "\"C:\Users\z035330\Documents\JJAUSSI\Other\JC\project DSBC\IT_243\repos\SSIS_PDS_slvd\LDSBC_IT243_xx\LDSBC_IT243_xx\LoadEXM_xx.dtsx\"" /CHECKPOINTING OFF /REPORTING E', 
+		@command=N'/FILE "\"C:\repository\SSIS_PDS_JR\LDSBC_IT243_JR\LoadDFNB3_JR.dtsx\"" /CHECKPOINTING OFF /REPORTING E', 
 		@database_name=N'master', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
@@ -68,4 +70,5 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
 GO
+
 
